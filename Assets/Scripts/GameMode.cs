b01cbc8ecab77eea790beef9904985c324d66831
Kurtenok <nikita.kurtenok@gmail.com>
2026-02-17 14:05:51 +0200

    Score added and Tiles improved

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -1,22 +1,36 @@
 using UnityEngine.EventSystems;
 using UnityEngine;
+using TMPro;
 
 public class GameMode : MonoBehaviour
 {
     [Header("Scene")]
     [SerializeField] private GameObject dicePrefab;
     [SerializeField] private Transform diceSpawnPoint;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
     [Header("Dice Control")]
     [SerializeField] private float sensitivity = 1;
     [SerializeField] private float maxOffset = 10f;
     [SerializeField] private float DicePushForce = 100;
 
+    [Header("Settings")]
+    [SerializeField] private float chanceOf4DiceSpawn=0.25f; // should be between 0 and 1
+
     private GameObject controlledDice;
-    private float startX;
+    private float startX; 
     private Vector2 startTouch;
+    private int currentScore=0;
+
+    void Awake()
+    {
+        chanceOf4DiceSpawn = Mathf.Clamp(chanceOf4DiceSpawn,0,1);
+
+        currentScore=0;
+    }
     void Start()
     {
-        
+        UpdateScoreText();
     }
 
     // Update is called once per frame
@@ -31,6 +45,11 @@ public class GameMode : MonoBehaviour
                 if(!dicePrefab) return;
 
                 controlledDice = Instantiate(dicePrefab,diceSpawnPoint.position,Quaternion.identity);
+
+                if(Random.Range(1,101)<=chanceOf4DiceSpawn*100)
+                {
+                    controlledDice.GetComponent<Dice>().SetDiceNum(4);
+                }
 
                 controlledDice.GetComponent<Rigidbody>().isKinematic = true;
 
@@ -64,5 +83,12 @@ public class GameMode : MonoBehaviour
                 rb.AddRelativeForce(transform.forward * DicePushForce);
             }
         }
+    }
+
+    public void UpdateScoreText()
+    {
+        if(!scoreText) return;
+
+        scoreText.text =$"Score: {currentScore}";
     }
 }
