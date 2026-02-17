9514f4ea30c1e02ed9fe8a1aec288d33ed44985b
Kurtenok <nikita.kurtenok@gmail.com>
2026-02-17 10:09:25 +0200

    Dices interaction added

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Dice : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private int diceNum = 2;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
     void Start()
     {
-       SetDiceNum(4);
+       SetDiceNum(2);
     }
 
     // Update is called once per frame
@@ -34,5 +41,18 @@ public class Dice : MonoBehaviour
     public void UpdateDiceView()
     {
         DiceManager.singleton.ChangeDiceView(this.transform,diceNum);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(!collision.gameObject.TryGetComponent<Dice>(out Dice dice)) return;
+        if(dice.GetDiceNum()!=diceNum) return;
+
+        Rigidbody otherRb= collision.gameObject.GetComponent<Rigidbody>();
+
+        if(otherRb.linearVelocity.magnitude>rb.linearVelocity.magnitude) return;
+
+        Destroy(collision.gameObject);
+        SetDiceNum((int)Mathf.Pow(diceNum,2));
     }
 }
